e2fda2ef6b7ffcea48e7c308d2043e8b81f27ed8
Isabelle Soupault <isabelle.soupault@gmail.com>
2021-05-25 11:45:30 +0200

    ajout des prises de notes de correction (à tester + tard)

--- script5.js
+++ script5.js
@@ -36,15 +36,35 @@ document.addEventListener("submit", function () {
     } else {
         let result = Math.trunc(firstNumber.value) * secondNumber.value;
         alert(result);
-        /* alert('toto' +
-            math.trunc(parseFloat(firstNumber.value)) * (parseFloat(secondNumber.value))); */
     }
 
-
-
     /*     else if (!Number.isInteger(secondNumber.value)) {
-            alert('Votre second chiffre doit contenir des chiffres ET une virgule (mais pas de point)')
+            alert('Votre second chiffre doit contenir des chiffres ET une virgule')
         } else {
             alert('Vos chiffres sont bien saisis !');
         }; */
 });
+
+
+//________________________//
+//                        //
+//      CORRECTION        //
+//________________________//
+
+/*
+
+Manipulation du DOM.
+1 - Rajouter dans le HTML un <p> <span> </span> </p>
+2 - Modifier mon else de la sorte:
+        - let result devient const result
+        - let displaySpan = document.getElementById('result");
+        - let strongElement = document.createElement('strong');
+        - strongElement.innerText = `Résultat : ${result};
+        - displaySpan.appendChild(strongElement);
+
+
+Ici, le strongElement va créer un élément strong dans mon DOM.
+Ensuite, le strongElement.innerText va indiquer le contenu qu'on souhaite récupérer.
+Et pour finit le displaySpan.appendChild(strongElement); va dire où on affiche exactement le contenu.
+
+/*
