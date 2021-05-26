6c86d2cd1d6a7100d99c3e8a262deffcea51c00a
Isabelle Soupault <isabelle.soupault@gmail.com>
2021-05-26 14:17:40 +0200

    correction exo2

--- Exo2/script2.js
+++ Exo2/script2.js
@@ -4,3 +4,18 @@ lossfocus.onblur = messageThx;
 function messageThx() {
     alert('Merci de votre participation');
 }
+
+//________________________//
+//                        //
+//      CORRECTION        //
+//________________________//
+
+/*
+perte de focus = quand on n'est plus sur le champ
+différence entre blur et onblur
+
+
+let lossfocus = document.getElementById('lastname');
+inputLastName.addEventListener('blur', () => alert('Merci de votre participation'));
+
+*/
